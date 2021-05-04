5be668f79aa404ccfec52586c84f2a553d2d7709
edwinmwiti <emwiti658@gmail.com>
2021-05-04 10:38:39 +0300

    Removed redundant header files

--- code/main/switch.h
+++
@@ -1,8 +0,0 @@
-#ifndef SWITCH-STATE
-#define SWITCH-STATE
-
-int switchState = LOW; // store the current state of the pin
-int ledState = LOW; // orange led indicator
-int reading;// state of the switch pin
-
-#endif
